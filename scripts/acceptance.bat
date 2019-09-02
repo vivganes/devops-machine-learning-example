e77e25bcc3c1c3d4ce1fb9f1c16f70dcd5e33988
ampyard-vivek-ganesan <vivek.ganesan@ampyard.com>
2019-09-02 18:23:52 +0530

    input

--- scripts/acceptance.bat
+++ scripts/acceptance.bat
@@ -1,4 +1,4 @@
 CALL conda.bat activate mybuild
 echo %CONDA_PREFIX%
 behave -f=json.pretty -o ./reports/integration.json
-python -m behave2cucumber ./reports/integration.json
+python -m behave2cucumber -i ./reports/integration.json

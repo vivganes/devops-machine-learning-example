14b1fbabad7f6a4a7ea52d683fd46b816c0880a2
ampyard-vivek-ganesan <vivek.ganesan@ampyard.com>
2019-09-02 17:51:24 +0530

    conda activate

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,2 +1,2 @@
 conda init bash
-activate mybuild && echo "hello" $CONDA_PREFIX &&v python -m pytest --verbose --junit-xml reports/unit_tests.xml
+conda activate mybuild && echo "hello" $CONDA_PREFIX && python -m pytest --verbose --junit-xml reports/unit_tests.xml
